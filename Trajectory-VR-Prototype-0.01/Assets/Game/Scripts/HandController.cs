415cd00ed372bfc719d1369480207391068e6af3
Tejas Shroff <tejasshroff24@gmail.com>
2018-03-27 23:14:24 -0400

    Pushed project out of subfolder an into separate repo

---
+++ Trajectory-VR-Prototype-0.01/Assets/Game/Scripts/HandController.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandController : MonoBehaviour {
+
+    public SteamVR_TrackedObject trackedObj;
+    private SteamVR_Controller.Device device;
+
+    public bool grabbedObj = false;
+
+    private Rigidbody rb;
+
+
+
+	// Use this for initialization
+	void Start ()
+    {
+        
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        device = SteamVR_Controller.Input((int)trackedObj.index);
+
+        
+
+        
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.tag == "cube")
+        {
+            if (device.GetPressDown(SteamVR_Controller.ButtonMask.Trigger))
+            {
+                GrabObject(other);
+            }
+
+            if (device.GetPressUp(SteamVR_Controller.ButtonMask.Trigger))
+            {
+                ThrowObject(other);
+            }
+        }
+    }
+
+    void GrabObject(Collider other)
+    {
+        Rigidbody cubeRb = other.gameObject.GetComponent<Rigidbody>();
+        cubeRb.isKinematic = true;
+        cubeRb.useGravity = false;
+        other.transform.parent = trackedObj.transform;
+        grabbedObj = true;
+        if (grabbedObj)
+        {
+            print("graddedObj true");
+        }
+    }
+
+    void ThrowObject(Collider other)
+    {
+        
+        Rigidbody cubeRb = other.gameObject.GetComponent<Rigidbody>();
+        cubeRb.isKinematic = false;
+        cubeRb.useGravity = true;
+
+        cubeRb.velocity = device.velocity;
+        cubeRb.angularVelocity = device.angularVelocity;
+
+        other.transform.parent = null;
+        grabbedObj = false;
+    }
+
+    
+}
---
+++ Trajectory-VR-Prototype-0.01/Assets/Game/Scripts/LocomotionFollow.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LocomotionFollow : MonoBehaviour {
+
+    public HandController leftHand;
+    public HandController rightHand;
+    public GameObject player; //cameraEye object
+
+    public float distanceFromPlayer;
+    private GameObject cube;
+
+	// Use this for initialization
+	void Start () {
+        cube = GameObject.Find("Cube");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (!leftHand.grabbedObj && !rightHand.grabbedObj)
+        {
+            Ray ray = new Ray(cube.transform.position, (player.transform.position - cube.transform.position));
+            Vector3 destPoint = ray.GetPoint(distanceFromPlayer);
+            Debug.DrawRay(cube.transform.position, (player.transform.position - cube.transform.position), Color.green, .1f);
+            transform.position = Vector3.Lerp(transform.position, new Vector3(destPoint.x, cube.transform.position.y - .25f, destPoint.z), Time.deltaTime);
+        }
+		
+	}
+}
